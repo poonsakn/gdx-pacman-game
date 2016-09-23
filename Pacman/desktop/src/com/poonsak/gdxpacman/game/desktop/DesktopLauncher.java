69f5d3a3108366e8713e69f1a5fcdc934b0310d2
poonsak <poonsak.nuch@gmail.com>
2016-09-23 11:37:52 +0700

    FirstC

---
+++ Pacman/desktop/src/com/poonsak/gdxpacman/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.poonsak.gdxpacman.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.poonsak.gdxpacman.game.PacmanDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 600;
+		new LwjglApplication(new PacmanDemo(), config);
+	}
+}
