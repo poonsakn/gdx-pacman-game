cdf4d81345743207fff1fdd278d8d747b9ff0677
poonsak <poonsak.n@ku.th>
2016-10-05 21:53:14 +0700

    minor text fixed

--- Pacman/desktop/src/com/poonsak/gdxpacman/game/desktop/DesktopLauncher.java
+++ Pacman/desktop/src/com/poonsak/gdxpacman/game/desktop/DesktopLauncher.java
@@ -2,13 +2,13 @@ package com.poonsak.gdxpacman.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.poonsak.gdxpacman.game.PacmanDemo;
+import com.poonsak.gdxpacman.game.PacmanGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 80;
+		config.width = 460;
 		config.height = 80;
-		new LwjglApplication(new PacmanDemo(), config);
+		new LwjglApplication(new PacmanGame(), config);
 	}
 }
