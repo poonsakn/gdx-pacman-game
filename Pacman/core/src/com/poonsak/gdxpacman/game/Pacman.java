8a7e83415fa44e6679a0b1c0e81120fe9bf06824
poonsak <poonsak.n@ku.th>
2016-10-05 23:10:42 +0700

    recreate pacman class

--- Pacman/core/src/com/poonsak/gdxpacman/game/Pacman.java
+++ Pacman/core/src/com/poonsak/gdxpacman/game/Pacman.java
@@ -1,5 +1,15 @@
 package com.poonsak.gdxpacman.game;
 
-public class Pacman {
+import com.badlogic.gdx.math.Vector2;
 
+public class Pacman {
+	private Vector2 position;
+	
+	public Pacman(int x, int y) {
+		position = new Vector2(x,y);
+	}
+	
+	public Vector2 getPosition() {
+		return position;
+	}
 }
