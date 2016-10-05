beb1daadbc8d9502653b4e24debd9a09c1fc3665
poonsak <poonsak.n@ku.th>
2016-10-06 00:51:15 +0700

    fixed move function

--- Pacman/core/src/com/poonsak/gdxpacman/game/Pacman.java
+++ Pacman/core/src/com/poonsak/gdxpacman/game/Pacman.java
@@ -3,6 +3,13 @@ package com.poonsak.gdxpacman.game;
 import com.badlogic.gdx.math.Vector2;
 
 public class Pacman {
+	
+	public static final int DIRECTION_UP = 1;
+    public static final int DIRECTION_RIGHT = 2;
+    public static final int DIRECTION_DOWN = 3;
+    public static final int DIRECTION_LEFT = 4;
+    public static final int DIRECTION_STILL = 0;
+    
 	private Vector2 position;
 	
 	public Pacman(int x, int y) {
@@ -12,4 +19,18 @@ public class Pacman {
 	public Vector2 getPosition() {
 		return position;
 	}
+	
+	private static final int [][] DIR_DIFF = new int [][] {
+		{0,0},
+		{0,-1},
+		{1,0},
+		{0,1},
+		{-1,0}
+	};
+	
+	public void move(int dir) { 
+        position.x += 10 * DIR_DIFF[dir][0];
+        position.y += 10 * DIR_DIFF[dir][1];
+        
+    }
 }
