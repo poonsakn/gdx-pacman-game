1ca4ef80c81408431763bf0c04d7d39d3e8ebb7f
poonsak <poonsak.n@ku.th>
2016-10-05 20:51:16 +0700

    dfgd

--- Pacman/desktop/src/com/poonsak/gdxpacman/game/desktop/DesktopLauncher.java
+++ Pacman/desktop/src/com/poonsak/gdxpacman/game/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import com.poonsak.gdxpacman.game.PacmanDemo;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 800;
-		config.height = 600;
+		config.width = 80;
+		config.height = 80;
 		new LwjglApplication(new PacmanDemo(), config);
 	}
 }
