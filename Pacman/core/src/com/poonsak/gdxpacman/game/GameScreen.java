7269e516919b07c4100969ab60e198bcb5c4e693
poonsak <poonsak.n@ku.th>
2016-10-06 21:33:36 +0700

    add World

--- Pacman/core/src/com/poonsak/gdxpacman/game/GameScreen.java
+++ Pacman/core/src/com/poonsak/gdxpacman/game/GameScreen.java
@@ -14,16 +14,18 @@ public class GameScreen extends ScreenAdapter {
 	
 //	private int x;
 //	private int y;
-	
+	World world;
 	private Pacman pacman;
 	
 	public GameScreen(PacmanGame pacmanGame) {
 		this.pacmanGame = pacmanGame;
 		pacmanImg = new Texture("pacman.png");
 		
-		pacman = new Pacman(100,100);
+//		pacman = new Pacman(100,100);
 //		x = 100;
 //		y = 100;
+		
+		world = new World(pacmanGame);
 	}
 	
 //	test
@@ -35,23 +37,23 @@ public class GameScreen extends ScreenAdapter {
 		
 		SpriteBatch batch = pacmanGame.batch;
 		batch.begin();
-		Vector2 pos = pacman.getPosition();
+		Vector2 pos = world.getPacman().getPosition();
 		batch.draw(pacmanImg, pos.x, pos.y);
 		batch.end();
 	}
 	
 	private void update(float delta) {
 		if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-			pacman.move(Pacman.DIRECTION_LEFT);
+			world.getPacman().move(Pacman.DIRECTION_LEFT);
 		}
 		if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-			pacman.move(Pacman.DIRECTION_RIGHT);
+			world.getPacman().move(Pacman.DIRECTION_RIGHT);
 		}
 		if(Gdx.input.isKeyPressed(Keys.UP)) {
-			pacman.move(Pacman.DIRECTION_UP);
+			world.getPacman().move(Pacman.DIRECTION_UP);
 		}
 		if(Gdx.input.isKeyPressed(Keys.DOWN)) {
-			pacman.move(Pacman.DIRECTION_DOWN);
+			world.getPacman().move(Pacman.DIRECTION_DOWN);
 		}
 	}
 }
